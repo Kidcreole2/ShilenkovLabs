8f04abfb2fada39e3ba39eb23eb92a4b02a3227e
Kidcreole2 <131256373+Kidcreole2@users.noreply.github.com>
2024-06-10 22:20:29 +0500

    Lab0

--- config/database.php
+++ config/database.php
@@ -40,25 +40,23 @@ return [
         ],
 
         'mysql' => [
-            'read' => [
-                'host' => [
-                    '192.168.1.1',
-                    '196.168.1.2',
-                ],
-            ],
-            'write' => [
-                'host' => [
-                    '196.168.1.3',
-                ],
-            ],
-            'sticky'    => true,
-            'driver'    => 'mysql',
-            'database'  => 'laravel',
-            'username'  => 'root',
-            'password'  => 'THE ONE YOU CREATED THE USER WITH',
-            'charset'   => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix'    => '',
+            'driver' => 'mysql',
+            'url' => env('DB_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
 
         'mariadb' => [
